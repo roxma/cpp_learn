eae8ab0303d207d6034d7ddc7732f5a4a7a30c44
roxma <roxma@qq.com>
2016-11-09 22:07:11 -0500

    add nix-env-setup.sh

---
+++ linux_env/nix-env-setup.sh
@@ -0,0 +1,7 @@
+
+# before executing this script,
+# install nix with `curl https://nixos.org/nix/install | sh`
+# The user installing nix needs sudo previlege
+
+nix-env --install python3
+pip install --user neovim
